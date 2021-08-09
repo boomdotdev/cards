3c28fcbb68caef4d883704ce5cf4a29bdab93273
Hristiyan Ivanov <civanov.me@gmail.com>
2021-08-09 13:10:26 +0300

    feat: add initial version

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.scss";
+import Card from "./components/Card";
+import eggplant from "./eggplant.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <Card
+        title="Okrr"
+        description="Only when a person knows what is wrong with him, only I know what is wrong with me"
+        icon={<img src={eggplant} alt="okrr" />}
+      />
     </div>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,65 @@
+import classNames from "classnames";
+import { motion } from "framer-motion";
+import { ReactElement, useState } from "react";
+import styles from "./Card.module.scss";
+
+interface CardProps {
+  title: string;
+  description?: string;
+  icon: ReactElement<HTMLImageElement>;
+}
+const transition = {
+  ease: [0.0, 0.8, 0.01, 1.01],
+  duration: 0.7,
+};
+const containerVariants = {
+  active: { scale: 1.1, zIndex: 1, transition },
+  inactive: { scale: 1, zIndex: 0, transition },
+};
+const descriptionVariants = {
+  visible: {
+    y: 0,
+    transition,
+  },
+  hidden: {
+    y: "100%",
+    transition,
+  },
+};
+
+export default function Card({ title, description, icon }: CardProps) {
+  const [active, setActive] = useState(false);
+
+  return (
+    <motion.div
+      className={classNames(styles.container, {
+        [styles.active]: active,
+      })}
+      onHoverStart={() => setActive(true)}
+      onHoverEnd={() => setActive(false)}
+      initial="inactive"
+      animate={active ? "active" : "inactive"}
+      variants={containerVariants}
+    >
+      <div className={styles.iconContainer}>{icon}</div>
+      <div className={classNames(styles.titleContainer)}>
+        <h5 className={styles.title}>{title}</h5>
+      </div>
+      <motion.div
+        initial="hidden"
+        animate={active ? "visible" : "hidden"}
+        variants={descriptionVariants}
+        className={classNames(styles.description)}
+      >
+        <div className={styles.descriptionInner}>
+          <div className={styles.descriptionIconContainer}>{icon}</div>
+          <div className={styles.descriptionLine}></div>
+          <div className={styles.descriptionTitleContainer}>
+            <h4 className={styles.descriptionTitle}>{title}</h4>
+          </div>
+          <p className={styles.descriptionText}>{description}</p>
+        </div>
+      </motion.div>
+    </motion.div>
+  );
+}
